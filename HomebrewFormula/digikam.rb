39ca9d723e36e7468d0e1c480129c3cad9704d36
Marvin Pinto <1159942+marvinpinto@users.noreply.github.com>
2024-12-16 04:17:26 -0500

    Update digikam to the newest available version

--- HomebrewFormula/digikam.rb
+++ HomebrewFormula/digikam.rb
@@ -3,20 +3,20 @@
 
 # Homebrew formula for Digikam
 class Digikam < Formula
-  APP_VERSION = "7.3.0"
+  APP_VERSION = "8.5.0"
   desc "Professional Photo Management with the Power of Open Source"
   homepage "https://www.digikam.org/"
   version APP_VERSION
 
   if OS.linux?
-    url "https://mirror.csclub.uwaterloo.ca/kde/stable/digikam/#{APP_VERSION}/digiKam-#{APP_VERSION}-x86-64.appimage"
-    sha256 "68c36c72102114e15ff29249806bb2bb4d389b4ceda7a9a4848412e25c174c57"
+    url "https://mirror.csclub.uwaterloo.ca/kde/stable/digikam/#{APP_VERSION}/digiKam-#{APP_VERSION}-Qt6-x86-64.appimage"
+    sha256 "dadb433bd7cc338a48c7c846bfd0a0c127756ce4652b5f27a895e45160049bc7"
   end
 
   def install
     prefix.install Dir["*"]
-    chmod(0755, "#{prefix}/digiKam-#{APP_VERSION}-x86-64.appimage")
-    bin.install_symlink("#{prefix}/digiKam-#{APP_VERSION}-x86-64.appimage" => "digikam")
+    chmod(0755, "#{prefix}/digiKam-#{APP_VERSION}-Qt6-x86-64.appimage")
+    bin.install_symlink("#{prefix}/digiKam-#{APP_VERSION}-Qt6-x86-64.appimage" => "digikam")
   end
 
   def caveats
